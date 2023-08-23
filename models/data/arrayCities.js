087b1541bb85d5828a0ba409a8bd14a0377bcdae
Sandra <sandranfernandez@gmail.com>
2023-08-23 16:31:33 -0300

    Entrega con requisitos mínimos.

--- models/data/arrayCities.js
+++ models/data/arrayCities.js
@@ -1,78 +1,96 @@
 const arrayCities =
-    [
-        {
-        "name":"Amsterdam",
-        "image":"https://images.pexels.com/photos/4456987/pexels-photo-4456987.jpeg",
-        "country":"",
-        "description":""
-        },
-        {
-        "name":"Bankok",
-        "image":"https://ak-d.tripcdn.com/images/1mi092224tzlig2ps30BA.jpg?proc=source/trip",
-        "country":"",
-        "description":""
-        },
-        {
-        "name":"Buenos Aires",
-        "image":"https://images.pexels.com/photos/13544086/pexels-photo-13544086.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
-        "country":"",
-        "description":""
-        },
-        {
-        "name":"ciudad del Cabo",
-        "image":"https://resources.formula-e.pulselive.com/photo-resources/2023/02/20/042dd057-7157-4069-beb8-2aa3af8b21ba/Cape-Town.jpg?width=1440&height=810",
-        "country":"",
-        "description":""
-        },
-        {
-        "name":"El Cairo",
-        "image":"https://images.pexels.com/photos/3290075/pexels-photo-3290075.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
-        "country":"",
-        "description":""
-        },
-        {
-        "name":"Kuala Lumpur",
-        "image":"https://paisajismourbano.com/wp-content/uploads/2022/05/Jardines-verticales-Kuala-Lumpur.jpg",
-        "country":"",
-        "description":""
-        },
-        {
-        "name":"Marrakech",
-        "image":"https://cdn.viviendodeviaje.com/wp-content/uploads/2023/02/02195738/consejos-viajar-marrakech.jpg",
-        "country":"",
-        "description":""
-        },
-        {
-        "name":"Nairobi",
-        "image":"https://www.andbeyond.com/wp-content/uploads/sites/5/giraffe-and-sky-line-nairobi.jpg",
-        "country":"",
-        "description":""
-        },
-        {
-        "name":"Paris",
-        "image":"https://images.pexels.com/photos/4039924/pexels-photo-4039924.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
-        "country":"",
-        "description":""
-        },
-        {
-        "name":"Quito",
-        "image":"https://i.pinimg.com/originals/fc/f4/d8/fcf4d8ae5db99a613329d03bff5e7d29.jpg",
-        "country":"",
-        "description":""
-        },
-        {
-        "name":"Seul",
-        "image":"https://imagenes.elpais.com/resizer/KWoTTW0OPg3VEZyjdtVA90ATKTI=/980x980/cloudfront-eu-central-1.images.arcpublishing.com/prisa/PO7ERGE7F3KLH4UCNHLAUCHIN4.jpg",
-        "country":"",
-        "description":""
-        },
-        {
-        "name":"Singapur",
-        "image":"https://images.pexels.com/photos/2804038/pexels-photo-2804038.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
-        "country":"",
-        "description":""
-        }
-    ]
+[
+	{
+		"name":"Cartagena",
+		"image":		"https://media.traveler.es/photos/61602c97709cbb2dfa86e2ef/16:9/w_1280,c_limit/iStock-1165965255%20copia.jpg",
+		"country":"Colombia",
+		"description":"Cartagena is not a city that is known but to which one returns. Its sweet Caribbean bustle, colonial attraction with a wall that adorns the horizon and encloses one of the most beautiful walled centers in the world, colorful streets full of history, culture, rhythm and energy and a great natural, gastronomic, nightlife and world-class hotel offer. , make it one of the main destinations in Latin America and the world."
+	},
+	{
+		"name":"Bruges",
+		"image":"https://www.visitbruges.be/sites/default/files/styles/full_width_desktop/public/media/tdms/122/images/engels-klooster.jpg.webp?itok=rd8Iw9VX",
+		"country":"Belgium",
+		"description":"Some places leave their mark on you, even though you can never quite fathom them. Bruges is one of those unique places. A city on a human scale, made great by its compelling history. Among other things, it was awarded the title of UNESCO World Heritage City. Exploring the historic city centre, it's immediately obvious why. Get to know Bruges even better and find out what's close to our hearts. You'll be pleasantly surprised by the delights the city has to offer."
+	},
+	{
+		"name":"Beijing",
+		"image":"https://r1.visitbeijing.com.cn/vbj-r/2018/0621/20180621040925732.jpg",
+		"country":"China",
+		"description":"The Summer Palace is a vast ensemble of lakes, gardens and palaces in Beijing. Mainly dominated by Longevity Hill and Kunming Lake, it covers an expanse of 2.9 square kilometers, three-quarters of which is water. In December 1998, UNESCO included the Summer Palace on its World Heritage List. It declared the Summer Palace 'a masterpiece of Chinese landscape garden design. The natural landscape of hills and open water is combined with artificial features such as pavilions, halls, palaces, temples...'"
+	},
+	{
+		"name":"Singapure",
+		"image":"https://www.visitsingapore.com/editorials/singapore-most-iconic-landmarks/_jcr_content/par/mobile_21_content_sl/sliderccpar1/editorial_generic_co.thumbnail.overview-image.730.411.jpg",
+		"country":"Singapure",
+		"description":"Singapore is a modern-day metropolis with an abundance of skyscrapers and iconic landmarks—come see Singapore through some of its most famous destinations, and gain insight into the stories behind them."
+	},
+	{
+		"name":"Seul",
+		"image":"https://english.visitseoul.net/comm/getImage?srvcId=MEDIA&parentSn=57135&fileTy=MEDIA&fileNo=1",
+		"country":"Republic of Korea",
+		"description":"This journey takes travelers along the old Seoul City Wall, which are 5–10 meters high and were erected to defend against marauding northern tribes during the Joseon era. Seoul City Wall Trail is 18.6 kilometers long and stretches from Namsan Mountain via the mountains of Ansan and Inwangsan to Bugaksan. The section from the gates of Changuimun to Hyehwamun is loaded with interesting scenery."
+	},
+	{
+		"name":"Paris",
+		"image":"https://parisjetaime.com/data/layout_image/fr-FR/La-Seine-%C3%A0-V%C3%A9lo-Coll%C3%A9giale-de-Mantes-la-Jolie-Cyclistes-V%C3%A9lo--630x405--%C2%A9-David-Darrault.jpg",
+		"country":"France",
+		"description":"Bicycle tourists embarking on the Seine à Vélo cycling route ride along the Seine, from Paris to Le Havre or from Paris to Deauville. Set off along car-free greenways, countryside paths and urban bike paths on your own, as a couple, with friends or family and cycle all the way to the sandy beaches of Normandy."
+	},
+	{
+		"name":"Nairobi",
+		"image":"https://d3qvqlc701gzhm.cloudfront.net/thumbs/29d7daaae2419c3d8bc798f8ae1c3a5ea7c4d2aa609a7836d084eceb9e71392e-750.jpg",
+		"country":"Kenya",
+		"description":"The city is rich in culture and history and is home to some of the most iconic sights in Kenya. The National Museum of Kenya is a great place to explore, as it offers a glimpse into the country’s history and culture. From there, head to the Nairobi National Park, where you can enjoy a safari and spot some local wildlife. For a more cultural experience, visit the Karen Blixen Museum, dedicated to the beloved author and surrounded by beautiful gardens."
+	},
+	{
+		"name":"Marrakech",
+		"image":"https://cdn.viviendodeviaje.com/wp-content/uploads/2023/02/02195738/consejos-viajar-marrakech.jpg",
+		"country":"Morocco",
+		"description":"From Marrakech, it is easy to make a few getaways that will give you a completely different view of Morocco. Whether towards the Atlantic coast or towards the mountains, you will be surprised by the diversity of the landscapes crossed, sometimes arid, sometimes luxuriant, and by the wealth of possibilities for activities."
+	},
+	{
+		"name":"Kula Lumpur",
+		"image":"https://paisajismourbano.com/wp-content/uploads/2022/05/Jardines-verticales-Kuala-Lumpur.jpg",
+		"country":"Malaysia",
+		"description":"Standing tall and majestic in the heart of Kuala Lumpur skyline is the PETRONAS Twin Towers. This quintessential structure is the inspiration of Tun Dr. Mahathir Mohamad’s vision and aspiration, which he ceremoniously officiated back in 1999. This iconic building is so rich in its history and is home to some of KL’s city attractions like Aquaria, Philharmonic Orchestra, SkyBridge, Petrosains and the Kuala Lumpur City Center (KLCC) mall."
+	},
+	{
+		"name":"Cairo",
+		"image":"https://images.pexels.com/photos/3290075/pexels-photo-3290075.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+		"country":"Egypt",
+		"description":"Situated by the Nile, Egypt’s capital Cairo holds some of the country’s best historical and contemporary offerings, lively streets that never sleep, and diverse neighborhoods. The dynamic metropolis features numerous heritage sites that imbue the city with a distinct charm and offer a glimpse into its Islamic and Coptic historie…"
+	},
+	{
+		"name":"Cape Town",
+		"image":"https://resources.formula-e.pulselive.com/photo-resources/2023/02/20/042dd057-7157-4069-beb8-2aa3af8b21ba/Cape-Town.jpg?width=1440&height=810",
+		"country":"South Africa",
+		"description":"Cape Town is a modern, cosmopolitan city surrounded by nature. Centred around Table Mountain, one of the New 7 Wonders of the world, the Mother City is known for its beautiful beaches, stunning biodiversity, award-winning food and wine, and sheer variety of experiences on offer."
+	},
+	{
+		"name":"Buenos Aires",
+		"image":"https://images.pexels.com/photos/13544086/pexels-photo-13544086.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+		"country":"Argentina",
+		"description":"The Galería Güemes is a true gem in the centre of Buenos Aires. This 100-metre shopping arcade, which opened in 1915 and unites the streets Florida and San Martín, conserves its European Art Nouveau-influenced architecture and captivates visitors with details such as its period elevators, glass dome and bronze-work."
+	},
+	{
+		"name":"Bangkok",
+		"image":"https://ak-d.tripcdn.com/images/1mi092224tzlig2ps30BA.jpg?proc=source/trip",
+		"country":"Thailand",
+		"description":"For major tourist attractions in Bangkok, most of them are Historical sites or religious sites such as temples in Bangkok. There is beautiful architecture, wall painting and also important in History, such as Wat Phra Sri Rattanasamaram (Phra Kaew), Wat Phra Chetuphon Wimon Mangalaram (wat pho), wat arun Ratchawararam, Wat Sra Ket, and Phra Bor Banphot (Golden mountain), Wat Ratchanadda, Wat Traimit, Wat Benchamabophit, Wat Bowonniwet Vihara Suthasan, Wat Thep Wararam etc. "
+	},
+	{
+		"name":"Amsterdam",
+		"image":"https://images.pexels.com/photos/4456987/pexels-photo-4456987.jpeg",
+		"country":"Kingdom of Netherlands",
+		"description":"With more than 800 years of history to discover, Amsterdam is rich with fascinating attractions and sights. From ancient castles to magnificent museums, secret courtyards to quaint cobbled streets, and the city's world-famous canals. Of course, no visit to Amsterdam is complete without the classics – along the way you’ll find all the tulips, windmills and cheese you could wish for."
+	},
+	{
+		"name":"Havana",
+		"image":"https://d14j9vfj10qgpp.cloudfront.net/media/b1a5548e-0311-4053-878e-bccf50ab2465.jpg",
+		"country":"Cuba",
+		"description":"Life in La Havana passes by surrounded by music, tradition, culture, national monuments and natural attractions of immeasurable value. Dances and Cuban “sones” spread to each one to this land’s children and to those who visit it as well; customs and traditions remain untouched and join new national and international celebrations to amaze visitors that plan a visit to La Havana, making it their Caribbean destination."
+	},
+]
 
 export default arrayCities
 
