9e333b21ba005b65e9f6293547e60ac107180035
Sandra <sandranfernandez@gmail.com>
2023-09-11 18:53:58 -0300

    JWT lito; passport en proceso.

--- controllers/auth/login.js
+++ controllers/auth/login.js
@@ -1,23 +1,34 @@
 import Users from "../../models/User.js";
 import bcrypt from "bcryptjs"
+import jwt from 'jsonwebtoken'
+import 'dotenv/config.js'
 
 export default async (req, res) =>{
     try {
 
-        const { email, password }= req.body
-        console.log(email)
-        const loginUser = await Users.findOne({email})
-        //Ya tengo middleware para validar el email
+        const { email: emailReq, password }= req.body
+        console.log(emailReq)
+        const loginUser = await Users.findOne({email: emailReq})
         console.log(loginUser)
+        
         if(!loginUser){
-            throw new Error("Invalid email o password")
+            throw new Error("Invalid email")
         }
         
         const validPassword = bcrypt.compareSync(password, loginUser.password)
         console.log(validPassword)
 
+        if(!validPassword) {
+            throw new Error("Invalid email o password")
+        }
+
+        const {name, lastName, email, photo, country} = loginUser
+
+        const token = jwt.sign({name, lastName, email, photo, country}, process.env.KEY) 
+
             return res.status(200).json({
                 success: true,
+                token,
                 loginUser
             })
         
